228ccb0e9683addd4130ad0204c2cde28a984849
cafefarm-soyeon <hayehfrp@gmail.com>
2024-12-10 21:10:57 +0900

    visual 커튼효과 추가, 배경 별 추가, id대신 useRef로 교체

--- src/components/Visual.jsx
+++ src/components/Visual.jsx
@@ -1,43 +1,58 @@
+import { useRef } from "react";
+
 const Visual = () => {
+  
+  const cloud1 = useRef(null);
+  const cloud2 = useRef(null);
+  const cloud3 = useRef(null);
+  const forest = useRef(null);
+  const forestBg = useRef(null);
+  const sun = useRef(null);
+  const bird = useRef(null);
+  const river = useRef(null);
+  const text = useRef(null);
 
   window.addEventListener('scroll', function(){
     let value = window.scrollY;
-    //console.log(value);
-    document.getElementById("cloud1").style.top = value * - 0.15 + "px";
-    document.getElementById("cloud2").style.top = value * - 0.15 + "px";
-    document.getElementById("cloud3").style.top = value * 0.4 + "px";
 
-    document.getElementById("forest").style.top = value * - 0.15 + "px";
-    document.getElementById("forestBg").style.height = value * 0.2 + 'px';
+    cloud1.current.style.setProperty('top', value * - 0.2 + "px");
+    cloud2.current.style.setProperty('top', value * - 0.1 + "px");
+    cloud3.current.style.setProperty('top', value * 0.4 + "px");
 
-    document.getElementById("sun").style.top = value * 0.7 + 'px';
-    document.getElementById("bird").style.top = value * 0.55 + 'px';
+    forest.current.style.setProperty('top', value * - 0.15 + "px");
+    forestBg.current.style.setProperty('height', value * 0.2 + "px");
 
-    document.getElementById("river").style.top = value * -0.06 + 'px';
-    document.getElementById("text").style.marginTop = value * -1.2 + 'px';
+    sun.current.style.setProperty('top', value * 0.7 + "px");
+    bird.current.style.setProperty('top', value * 0.65 + "px");
 
-    // cloud1.style.top = value * 0.3 + 'px';
-    // cloud2.style.top = value * 0.2 + 'px';
-    // cloud3.style.top = value * 0.7 + 'px';
+    river.current.style.setProperty('top', value * -0.15 + "px");
+    text.current.style.setProperty('margin-top', value * 0.7 + "px");
 
   });
 
   return(
     <>
+    <section className="curtain">
+      <div className="left"></div>
+      <div className="right"></div>
+    </section>
     <section className="part visual">
-      <img src="../../public/image/cloud1.png" id="cloud1" alt="구름1" />
-      <img src="../../public/image/cloud2.png" id="cloud2" alt="구름2" />
-      <img src="../../public/image/cloud3.png" id="cloud3" alt="구름3" />
-      <img src="../../public/image/sun.png" id="sun"alt="해" />
-      <img src="../../public/image/mountain1.png" id="mountain1" alt="산1" />
-      <p className="visual-text" id="text">Weclome to the world</p>
-      <img src="../../public/image/mountain2.png" id="mountain2" alt="산2" />
-      <img src="../../public/image/river.png" id="river" alt="강" />
-      <img src="../../public/image/mountain3.png" id="mountain3" alt="산3" />
-      <img src="../../public/image/mountain4.png" id="mountain4" alt="산4" />
-      <img src="../../public/image/forest.png" id="forest" alt="숲"  />
-      <div id="forestBg"></div>
-      <img src="../../public/image/bird.png" id="bird" alt="새" />
+      <img src="../../public/image/cloud1.png" alt="구름1" ref={cloud1} />
+      <img src="../../public/image/cloud2.png" alt="구름2" ref={cloud2} />
+      <img src="../../public/image/cloud3.png"  alt="구름3" ref={cloud3} />
+      <div className="space stars1 "></div>
+      <div className="space stars2"></div>
+      <div className="space stars3"></div>
+      <img src="../../public/image/sun.png" alt="해" ref={sun}/>
+      <img src="../../public/image/mountain1.png" alt="산1" />
+      <img src="../../public/image/mountain2.png" alt="산2" />
+      <p className="visual-text" ref={text} >Soyeon&#39;s Home</p>
+      <img src="../../public/image/river.png" alt="강" ref={river} />
+      <img src="../../public/image/mountain3.png" alt="산3" />
+      <img src="../../public/image/mountain4.png" alt="산4" />
+      <img src="../../public/image/forest.png" className="forest" alt="숲" ref={forest}  />
+      <div className="forestBg" ref={forestBg}></div>
+      <img src="../../public/image/bird.png" className="bird" alt="새" ref={bird} />
     </section>
     </>
   );
