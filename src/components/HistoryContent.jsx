ea615f4ce847c9480bc7aa3d8855cf9d31a9f4c9
cafefarm-soyeon <hayehfrp@gmail.com>
2024-12-22 20:09:02 +0900

    history 구조수정 및 스타일 추가

--- src/components/HistoryContent.jsx
+++ src/components/HistoryContent.jsx
@@ -2,14 +2,20 @@ const HistoryContent = ( { company, position, service, team, work, set }) => {
   return(
     <>
       <div className="history-cont">
-        <h2 className="fs-18 fw-bold pt_10">{company}</h2>
+        <h2>
+          <span className="company">{company}  </span>
+          | <span className="position">{position}</span>
+          <p className="date">{service.date} ( {service.sum} )</p>
+        </h2>
         <ul className="flex flex-column">
-          <li>{position}</li>
-          <li>{service.date} | {service.sum}</li>
-          <li></li>
-          <li>{team}</li>
-          <li>{work}</li>
-          <li>{set.map((item)=> <p className="fs-14" key={item}>{item}</p>)}</li>
+          <li className="team"><p className="s-title">부서</p> {team}</li>
+          <li className="work"><p className="s-title">주요업무</p> {work}</li>
+          <li className="use">
+            <p className="s-title">작업환경</p>
+            <p className="flex flex-wrap">
+            {set.map((item)=> <span key={item}>{item}</span>)}
+            </p>
+            </li>
         </ul>
       </div>
     </>
